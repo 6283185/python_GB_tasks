2ed82ca0445316b5143d1361a4a586e2b7270c17
Dmitry <chernyshov85@mail.ru>
2023-09-18 18:13:30 +0300

    hw12

--- student.py
+++ student.py
@@ -1,135 +1,129 @@
-
-from typing import Any
-from string import ascii_letters
-import random
-from serialize.s_csv import read_l_csv
-from serialize.s_json import write_json
+import csv
+from functools import reduce
+from pathlib import Path
 
 
-class TitleWord:
-    '''Класс дескриптор, контролирующий, что ФИО состоит только из букв и начинаеетсся с заглаавной буквы'''
+class Validate:
+    "Дескриптор на проверку в ФИО наличия первой заглавной буквы, а также что ФИО состоит только из букв"
 
-    RUS_LETTERS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+    def __set_name__(self, owner, name):
+        self.param_name = '_' + name
 
-    @classmethod
-    def validate(cls, value: str):
-        if not value.istitle():
-            raise ValueError('ФИО должно быть с заглавной буквы')
-        if len(value.strip(ascii_letters + cls.RUS_LETTERS)) != 0:
-            raise ValueError('ФИО должно должно сотоять только из букв')
+    def __get__(self, instance, owner):
+        return getattr(instance, self.param_name)
 
-    def __set_name__(self, owner, name) -> None:
-        self.name = '_' + name
-
-    def __get__(self, instance, owner) -> Any:
-        return getattr(instance, self.name)
-
-    def __set__(self, instance, value) -> None:
+    def __set__(self, instance, value):
         self.validate(value)
-        setattr(instance, self.name, value)
+        setattr(instance, self.param_name, value)
+
+    def validate(self, value):
+        if not value.isalpha():
+            raise TypeError(f'Значение {value} должно содержать только буквы')
+        if not value.istitle():
+            raise TypeError(f'Значение {value} должно начинаться с заглавной буквы')
 
 
 class Student:
-    MIN_MARK = 2
-    MAX_SUBJECT_MARK = 5
-    MAX_TEST_MARK = 100
+    name = Validate()
+    second_name = Validate()
+    surname = Validate()
+    _lessons = None
 
-    first_name = TitleWord()
-    middle_name = TitleWord()
-    last_name = TitleWord()
+    def __init__(self, name: str, second_name: str, surname: str, lessons_file: Path):
+        self.name = name
+        self.second_name = second_name
+        self.surname = surname
+        self.lessons = lessons_file
 
-    subjects = tuple()
+    @property
+    def lessons(self):
+        return self._lessons
 
-    def __init__(self, first_name: str, middle_name: str, last_name: str, subjects_file='subjects.csv') -> None:
-        self.first_name = first_name
-        self.middle_name = middle_name
-        self.last_name = last_name
+    @lessons.setter
+    def lessons(self, lessons_file: Path):
+        " Декоратор считывает с файла список предметов в словарь lessons"
 
-        self.subjects = tuple(sub[0] for sub in read_l_csv(subjects_file))
-        self.perfomance_journal = {
-            subject: {'marks': [], 'test_marks': [], 'avg_mark': None, 'avg_test_mark': None} for subject in self.subjects}
-        self.perfomance_journal['total_avg_mark'] = None
-        self.perfomance_journal['total_avg_test_mark'] = None
+        self._lessons = {"lessons": {}}
+        with open(lessons_file, 'r', encoding='utf-8') as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader:
+                self._lessons["lessons"][row[0]] = {"assessments": [], "test_results": [], "average_test": None}
+        self._lessons["middle_assessment"] = None
+        self._lessons["middle_test"] = None
 
-    def __str__(self) -> str:
-        '''Отображает информацию об успеваемости студента'''
-        line = '-'*70
-        subject_stat = '\n'.join([f"{sub:<20}\t{self.perfomance_journal[sub]['avg_mark']:<15}\t{self.perfomance_journal[sub]['avg_test_mark']:<15}"
-                                  for sub in self.subjects])
-        title = '{:^20}\t{:^15}\t{:^15}\n{}'.format(
-            'Предмет', 'Средняя оценка', 'Средняя оценка за тесты', line)
-        total_avgs = f"\t{self.perfomance_journal['total_avg_mark']:3.1f}\t{self.perfomance_journal['total_avg_test_mark']:3.1f}"
-        footer = f"Итого по всем предметам:\nСредняя оценка\tСредняя оценка за тесты\n{line}\n{total_avgs}\n{line}"
-        return f'{line}\nСтудент: {self.first_name} {self.middle_name} {self.last_name}\n{line}\n{title}\n{subject_stat}\n{line}\n{footer}'
+    def __call__(self, lesson: str, number: int, type_est: str = "lesson"):
+        "Метод для сохранения новой оценки по предмету/тесту"
 
-    def __repr__(self) -> str:
-        class_name = type(self).__name__
-        return f'{class_name}({self.first_name}, {self.middle_name}, {self.last_name})'
+        if lesson not in self.lessons["lessons"].keys():
+            raise AttributeError("Нет такого предмета")
+        if type_est == "lesson":
+            if number < 2 or number > 5:
+                raise ValueError("Оценка должна быть от 2 до 5")
+            self.lessons["lessons"][lesson]["assessments"].append(number)
+            self.lessons["middle_assessment"] = self.calculate_middle_estimate(self.lessons)
+        elif type_est == "test":
+            if number < 0 or number > 100:
+                raise ValueError("Оценка должна быть от 0 до 100")
+            self.lessons["lessons"][lesson]["test_results"].append(number)
+
+            # средний бал по тестам по каждому предмету:
+            self.lessons["lessons"][lesson]["average_test"] = reduce(lambda x, y: x + y, self.lessons["lessons"][lesson]["test_results"]) / len(self.lessons["lessons"][lesson]["test_results"])
+            self.lessons['middle_test'] = self.calculate_middle_test(self.lessons)
 
     @staticmethod
-    def validate_mark(value: int, min_mark: int = 0, max_mark: int = 5) -> None:
-        '''Проверяет, что оценка в диапазоне (min_mark - max_mark)'''
+    def calculate_middle_estimate(lessons: dict) -> float:
+        "Подсчет средней оценки по всем предметам"
 
-        if type(value) is not int or value < min_mark or value > max_mark:
-            raise ValueError(
-                f'Оценка должна быть целым числом в диапазоне {min_mark} - {max_mark}')
+        all_estimates = []
+        [all_estimates.extend(lessons["lessons"][name]["assessments"]) for name in lessons["lessons"]]
 
-    def set_subject_mark(self, subject: str, mark: int, is_test=False):
-        '''Добавляет оценку по предмету или тесту в журнал'''
+        return reduce(lambda x, y: x + y, all_estimates) / len(all_estimates)
 
-        if subject not in self.subjects:
-            raise ValueError("Предмет не входит в программу обучения")
+    @staticmethod
+    def calculate_middle_test(lessons: dict) -> float:
+        "Подсчет среднего бала по тестам по всем предметам"
 
-        self.validate_mark(
-            mark, max_mark=self.MAX_TEST_MARK) if is_test else self.validate_mark(mark, min_mark=self.MIN_MARK)
+        all_estimates = []
+        [all_estimates.extend(lessons["lessons"][name]["test_results"]) for name in lessons["lessons"]]
 
-        subject_mark = 'test_marks' if is_test else 'marks'
-        self.perfomance_journal[subject][subject_mark].append(mark)
-        self._update_subject_stat(subject, is_test)
-        self._update_total_avg(is_test)
+        return reduce(lambda x, y: x + y, all_estimates) / len(all_estimates)
 
-    def _update_subject_stat(self, subject: str, is_test: bool = False) -> None:
-        '''Обновляет средние оценки по предмету'''
-        subject_marks, avg_subject_mark = (
-            'test_marks', 'avg_test_mark') if is_test else ('marks', 'avg_mark')
-        self.perfomance_journal[subject][avg_subject_mark] = sum(
-            self.perfomance_journal[subject][subject_marks]) / len(self.perfomance_journal[subject][subject_marks])
+    def __repr__(self):
+        result = f'ФИО студента = {self.name} {self.second_name} {self.surname}\nСредняя оценка по всем предметам = {self.lessons["middle_assessment"]}\nСредний бал по всем тестам = {self.lessons["middle_test"]}\n'
+        result += "\nОценки по предметам:\n"
 
-    def _update_total_avg(self, is_test):
-        '''Обновляет общую среднюю оценку по всем предметам'''
-        avg_subject_mark, total_avg_mark = (
-            'avg_test_mark', 'total_avg_test_mark') if is_test else ('avg_mark', 'total_avg_mark')
+        for key, value in self.lessons["lessons"].items():
+            result += f'{key} = {value["assessments"]}\n'
+        result += "\nТесты по предметам:\n"
+        for key, value in self.lessons["lessons"].items():
+            result += f'{key} = {value["test_results"]}, ср.бал = {value["average_test"]}\n'
 
-        total_avg = 0.
-        for key, val in self.perfomance_journal.items():
-            if key not in ('total_avg_test_mark', 'total_avg_mark'):
-                for sub_key, sub_val in val.items():
-                    if sub_key == avg_subject_mark:
-                        if sub_val is not None:
-                            total_avg += sub_val
-        self.perfomance_journal[total_avg_mark] = total_avg / \
-            len(self.subjects)
-
-    def export_journal_to_json(self, file_name: str = 'perfomance_journal', preaty_print: bool = False):
-        '''Экспортирует журнал успеваемости в json файл'''
-        export_file = f"{file_name}.json"
-        write_json(st.perfomance_journal, export_file, preaty_print)
-
-
-def fill_random_journal(st: Student, tests_count: int) -> None:
-    '''Заполняет журнал успеваемости случайными оценками для тестирования'''
-    for sub in st.subjects:
-        for _ in range(tests_count):
-            st.set_subject_mark(sub,
-                                random.randrange(2, 5 + 1))
-        for _ in range(tests_count // 8):
-            st.set_subject_mark(sub,
-                                random.randrange(100 + 1), is_test=True)
+        return result
 
 
 if __name__ == '__main__':
-    st = Student('Иван', 'Петрович', 'Кузнецов')
-    fill_random_journal(st, tests_count=20)
-    st.export_journal_to_json()
-    print(f'{st = }')
-    print(st)
+    student_1 = Student("Alexey", "Alexeevich", "Nesterov", Path('lessons.csv'))
+    student_1("русский язык", 3)
+    student_1("русский язык", 5)
+    student_1("физика", 5)
+    student_1("математика", 5)
+    student_1("информатика", 35, "test")
+    student_1("математика", 99, "test")
+    student_1("химия", 50, "test")
+    student_1("химия", 4)
+    student_1("информатика", 4)
+    student_1("информатика", 40, "test")
+    print(student_1)
+
+    student_2 = Student("Ivan", "Ivanjvich", "Ivanov", Path('lessons.csv'))
+    student_2("русский язык", 4)
+    student_2("история", 5)
+    student_2("физика", 3)
+    student_2("математика", 5)
+    student_2("история", 40, "test")
+    student_2("история", 98, "test")
+    student_2("химия", 4)
+    student_2("информатика", 3)
+    student_2("информатика", 55, "test")
+    student_2("история", 55, "test")
+    print(student_2)
